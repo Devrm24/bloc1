27a68706390f152ae1173f98fc57f258f034ef95
Devrm24 <asimsek@info.local>
2024-12-06 15:33:06 +0100

    encore reglage de probleme

--- docs/js/TD4/exercice.js
+++ docs/js/TD4/exercice.js
@@ -1,17 +1,16 @@
 // Exercice 1 :
 
 function changeColor(color) { 
-   document.body.style.background = color; 
+    document.body.style.background = color; 
 }
 
 document.addEventListener("DOMContentLoaded",function(){
 
-   const inputElement = document.getElementById("inputElement");
-   const ajouterBtn = document.getElementById("ajouterBtn");
-       
-   const liste = document.getElementById("liste");
+    const inputElement = document.getElementById("inputElement");
+    const ajouterBtn = document.getElementById("ajouterBtn");
+        
+    const liste = document.getElementById("liste");
 
-<<<<<<< HEAD:docs/js/TD4/exercice.js
     ajouterBtn.addEventListener("click",
 
         function(){
@@ -19,73 +18,57 @@ document.addEventListener("DOMContentLoaded",function(){
             if(texte !== ""){
                 const li = document.createElement("li");
                 li.textContent = texte;
-=======
-   ajouterBtn.addEventListener("click",
->>>>>>> 775c97f088e30df4d07db73802d1ad6329456911:docs/js-td4/exercice.js
 
-       function(){
-           const texte = inputElement.value;
-           if(texte !== ""){
-               const li = document.createElement("li");
-               li.textContent = texte;
+                liste.appendChild(li);
+                inputElement.value = "";
+            }
+        });
 
-               liste.appendChild(li);
-               inputElement.value = "";
-           }
-       });
+    const survolElement = document.getElementById("survolElement");
 
-   const survolElement = document.getElementById("survolElement");
-
-   survolElement.addEventListener("mouseover",
-       function(){
-           alert("Vous avez survolé l'élement ! ");
-       });
-       
-   
+    survolElement.addEventListener("mouseover",
+        function(){
+            alert("Vous avez survolé l'élement ! ");
+        });
+        
+    
 });
 
 function somme(){
-<<<<<<< HEAD:docs/js/TD4/exercice.js
     let nbr1, nbr2, sum;
         nbr2 = Number(document.getElementById("nbr2").value);
         sum = nbr1 + nbr2;
         document.getElementById("sum").value = sum;
-=======
-   let nbr1, nbr2, sum;
-       nbr2 = Number(document.getElementById("nbr2").value);
-       sum = nbr1 + nbr2;
-       document.getElementById("sum").value = sum;
->>>>>>> 775c97f088e30df4d07db73802d1ad6329456911:docs/js-td4/exercice.js
 }
 
 function changeit(val) {
-   document.getElementById("hidden").style.display="none";
-   document.getElementById("hidden2").style.display="none";
-   document.getElementById("hidden3").style.display="none";
-   document.getElementById(val).style.display="block";
+    document.getElementById("hidden").style.display="none";
+    document.getElementById("hidden2").style.display="none";
+    document.getElementById("hidden3").style.display="none";
+    document.getElementById(val).style.display="block";
 }
 
 function refresh(){
-   var t = 1000; // rafraîchissement en millisecondes
-   setTimeout('showDate()',t)
+    var t = 1000; // rafraîchissement en millisecondes
+    setTimeout('showDate()',t)
 }
 
 function showDate() {
-   let date = new Date()
-   let h = date.getHours();
-   let m = date.getMinutes();
-   let s = date.getSeconds();
-   if( h < 10 ){ h = '0' + h; }
-   if( m < 10 ){ m = '0' + m; }
-   if( s < 10 ){ s = '0' + s; }
-   let time = h + ':' + m + ':' + s
-   document.getElementById('horloge').innerHTML = time;
-   refresh();
-}
+    let date = new Date()
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    if( h < 10 ){ h = '0' + h; }
+    if( m < 10 ){ m = '0' + m; }
+    if( s < 10 ){ s = '0' + s; }
+    let time = h + ':' + m + ':' + s
+    document.getElementById('horloge').innerHTML = time;
+    refresh();
+ }
 
-
+ 
 function ConfirmerAge(){
 if (confirm("Confirmez vous avoir "+ formulaire.age.value +" ans ?")){
-   formulaire.submit();
-   }
+    formulaire.submit();
+    }
 }
